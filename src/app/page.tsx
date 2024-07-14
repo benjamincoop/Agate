67ac23444a4bf169d7b1d97c2697b4bc8983f9b7
Benjamin Cooper <benjaminbantamcooper@gmail.com>
2024-07-14 13:31:46 -0500

    added react-icons and created nav menu component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,10 @@
-import { Button } from "@/components/ui/button"
+import NavMenu from "@/components/nav-menu/nav-menu";
 import SpiralDecor from "@/components/spiral-decor/spiral-decor";
-import {
-  Sheet,
-  SheetClose,
-  SheetContent,
-  SheetDescription,
-  SheetFooter,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet"
  
 export default function Home() {
   return (
     <>
-      <Sheet>
-        <SheetTrigger asChild>
-          <Button variant="outline">Open</Button>
-        </SheetTrigger>
-        <SheetContent side='left'>
-          <SheetHeader>
-            <SheetTitle>Navigation</SheetTitle>
-            <SheetDescription>
-              Put some navigation links here
-            </SheetDescription>
-          </SheetHeader>
-          <SheetFooter>
-          </SheetFooter>
-        </SheetContent>
-      </Sheet>
+      <NavMenu></NavMenu>
       <SpiralDecor></SpiralDecor>
     </>
   )
